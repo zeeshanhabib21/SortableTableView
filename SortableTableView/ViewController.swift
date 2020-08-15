ddaef7a1375c8c3bb899b1760d58a5fc7e5cdfb4
Zeeshan Habib <zeshanhabib@icloud.com>
2020-08-15 14:50:25 +0500

    Finish Code

--- SortableTableView/ViewController.swift
+++ SortableTableView/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var tableView: UITableView!
+    
+    var item =
+    [
+      "First","Second","Third"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
 
+}
+extension ViewController: UITableViewDataSource,UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sortCell", for: indexPath)
+        cell.textLabel?.text = item[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Arial", size: 22)
+        return cell
+    }
+    
+    
+    @IBAction func didTapSort()
+    {
+        if tableView.isEditing{
+            tableView.isEditing = false
+        }else
+        {
+            tableView.isEditing = true
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        item.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+    
+    
+    
 }
 
